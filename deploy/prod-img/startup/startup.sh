f66e21a1f2c384f5ef6e50642ac15177704556c6
asnte <kensei.352@gmail.com>
2021-06-27 20:54:53 +0900

    警告で表示されていた内容の解消

--- deploy/prod-img/startup/startup.sh
+++ deploy/prod-img/startup/startup.sh
@@ -3,9 +3,12 @@
 # アプリケーションディレクトリに移動
 cd /var/www/backend
 
+# ユーザーの追加
+useradd flask-user
+
 # uwsgi起動
-# TODO: rootで動かさない方が良いですよ。というWarningが出るので、ちゃんと uid 等を指定した方が良い
-uwsgi --socket 127.0.0.1:3031 --chdir /var/www/backend/ --wsgi-file main.py --callable app --master --processes 4 --threads 2 & 
+# 後ろの $ + 半角スペース は必須（理由は不明だが無い場合はバックエンド側が正常に立ち上がらない）
+uwsgi --ini uwsgi.ini & 
 
 # nginx起動
 nginx -g "daemon off;"
